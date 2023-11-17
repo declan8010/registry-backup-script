3152d28701e0c05e02b0357d42e4afe9d4c4ac75
DeclanE <declanemery6@gmail.com>
2023-11-17 18:20:26 +0000

    Backup Registry to path with date

--- registry-backup-tool.bat
+++ registry-backup-tool.bat
@@ -1,1 +1,15 @@
 @echo off
+setlocal
+
+:: Get the current date
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
+for /f "tokens=%datetime:~4,2% delims=," %%M in ("Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec") do set month=%%M
+set date=%datetime:~6,2%-%month%-%datetime:~0,4%
+
+:: Prompt the user for the backup path
+set /p backup_path="Enter the path where you want the backup to be saved: "
+
+:: Export the registry
+regedit /E "%backup_path%\registry_backup_%date%.reg"
+
+endlocal
