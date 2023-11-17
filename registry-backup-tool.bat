29a58a96b03fdfbae7d074b8e5528608742796fe
DeclanE <declanemery6@gmail.com>
2023-11-17 18:29:17 +0000

    Added output value.

--- registry-backup-tool.bat
+++ registry-backup-tool.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal
 
+:: Prompt the user for the backup path
+set /p backup_path="Enter the path where you want the backup to be saved: "
+
 :: Get the current date
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
 for /f "tokens=%datetime:~4,2% delims=," %%M in ("Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec") do set month=%%M
 set date=%datetime:~6,2%-%month%-%datetime:~0,4%
-
-:: Prompt the user for the backup path
-set /p backup_path="Enter the path where you want the backup to be saved: "
-
+echo.
 :: Check if the file already exists
 if exist "%backup_path%\Registry_Backup_%date%.reg" (
     set /p overwrite="A file with the same name already exists. Do you want to overwrite it? (y/n): "
@@ -18,7 +18,12 @@ if exist "%backup_path%\Registry_Backup_%date%.reg" (
     )
 )
 
+echo Please wait while the backup is being taken. This may take some time.
 :: Export the registry
 regedit /E "%backup_path%\Registry_Backup_%date%.reg"
+echo.
+echo Backup saved to: "%backup_path%\Registry_Backup_%date%.reg"
 
 endlocal
+echo.
+pause
